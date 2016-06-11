a0f857a2d3f95165ca91ce7ae0371d77a34e77ab
Bill Robertson <billrobertson42@gmail.com>
2016-06-11 18:55:46 -0400

    upgrade versions

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject soupup "0.1.0-SNAPSHOT"
+(defproject soupup "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.jsoup/jsoup "1.7.3"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.jsoup/jsoup "1.9.2"]]
   :profiles {:dev {:dependencies [[hiccup "1.0.5"]]}}
 )
